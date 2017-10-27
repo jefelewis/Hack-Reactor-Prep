dbe5e954e9fa3c6831e0d76776acd14450f0b650
Jeff Lewis <jeffylew77@gmail.com>
2017-10-26 19:05:58 -0700

    Create ObjectToArrayPart2.js

---
+++ Module 0/ObjectToArrayPart2.js
@@ -0,0 +1,30 @@
+/*
+Instructions:
+Write a function called "listAllValues" which returns an newArray of all the input object’s values.
+
+Input (Object):
+var object = {name: "Krysten", age: 33, hasPets : false};
+
+Output:
+["Krysten", 33, false]
+
+Starter Code:
+var object = {name : "Krysten", age : 33, hasPets : false};
+
+function listAllValues(obj) {
+	// your code here
+}
+
+*/
+
+var object = {name : "Krysten", age: 33, hasPets: false};
+
+function listAllValues(obj) {
+	var newArray = [];
+	for(var x in obj) {
+		newArray.push(obj[x]);
+	}
+return newArray; 
+} 
+
+listAllValues(object);
