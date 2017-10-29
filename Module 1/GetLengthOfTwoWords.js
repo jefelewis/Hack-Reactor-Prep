b13e0f1bb63dc167d0ca77fb443006a2199afff8
Jeff Lewis <jeffylew77@gmail.com>
2017-10-29 15:39:33 -0700

    Create GetLengthOfTwoWords.js

---
+++ Module 1/GetLengthOfTwoWords.js
@@ -0,0 +1,28 @@
+/*
+Instructions:
+Write a function called "getLengthOfTwoWords".
+
+Given 2 words, "getLengthOfTwoWords" returns the sum of their lengths.
+
+Input:
+getLengthOfTwoWords('some', 'words');
+
+Output:
+9
+
+
+Starter Code:
+function getLengthOfTwoWords(word1, word2){
+	// your code here
+}
+*/
+
+// Answer:
+function getLengthOfTwoWords(word1, word2){
+	return word1.length + word2.length
+}
+
+getLengthOfTwoWords("Wave", "Surf");
+
+// Output
+8
