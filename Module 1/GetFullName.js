09c687d414054cb426f773595b162d0be4a9b39c
Jeff Lewis <jeffylew77@gmail.com>
2017-10-29 15:27:35 -0700

    Create GetFullName.js

---
+++ Module 1/GetFullName.js
@@ -0,0 +1,28 @@
+/*
+Instructions:
+Write a function called "getFullName".
+
+Given a first and a last name, "getFullName" returns a single string with the given first and last names separated by a single space.
+
+Input:
+getFullName("Joe", "Smith");
+
+Output:
+"Joe Smith"
+
+
+Starter Code:
+function getFullName(firstName, lastName){
+	// your code here
+}
+*/
+
+Answer:
+function getFullName(firstName, lastName){
+	return firstName + " " +  lastName;
+}
+
+getFullName("Jeff", "Lewis");
+
+// Output
+Jeff Lewis
