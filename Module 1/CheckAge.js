d39029ba2e1ec52712f09165aff6f8491c51b28f
Jeff Lewis <jeffylew77@gmail.com>
2017-10-29 15:24:51 -0700

    Create CheckAge.js

---
+++ Module 1/CheckAge.js
@@ -0,0 +1,38 @@
+/*
+Instructions:
+Write a function called "checkAge". 
+
+Given a person's name and age, "checkAge" returns one of two messages:
+"Go home, {insert_name_here}!", if they are younger than 21.
+"Welcome, {insert_name_here}!", if they are 21 or older.
+
+Naturally, replace "{insert_name_here}" with the given name
+
+Input:
+checkAge("Adrian", 22);
+
+Output:
+'Welcome, Adrian!
+
+
+Starter Code:
+function checkAge(name, age){
+	// your code here
+}
+*/
+
+
+// Answer:
+function checkAge(name, age) {
+	if(age >= 21){
+		return "Welcome, " + name + "!"
+	}
+	else {
+		return "Go home, " + name + "!"
+	}
+}
+
+checkAge("Jeff", 25);
+
+// Output
+Welcome, Jeff!
