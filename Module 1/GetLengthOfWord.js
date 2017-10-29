30ae0c7ac364ef7ea15ade3598c2a926b61d2799
Jeff Lewis <jeffylew77@gmail.com>
2017-10-29 15:28:38 -0700

    Create GetLengthOfWord.js

---
+++ Module 1/GetLengthOfWord.js
@@ -0,0 +1,27 @@
+/*
+Instructions:
+Write a function called "getLengthOfWord".
+
+Given a word, "getLengthOfWord" returns the length of the given word.
+
+Input:
+getLengthOfWord("some");
+
+Output:
+4
+*/
+
+Starter Code:
+function getLengthOfWord(word){
+	// your code here
+}
+
+// Answer:
+function getLengthOfWord(word){
+	return word.length;
+}
+
+getLengthOfWord("word")
+
+// Output
+4
