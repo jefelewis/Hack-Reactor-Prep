1ef7c65b5c82743e34f706aabcac39c95a88db66
Jeff Lewis <jeffylew77@gmail.com>
2017-10-30 17:59:00 -0700

    Create 013_IsLessThan30.js

---
+++ Module 1/013_IsLessThan30.js
@@ -0,0 +1,32 @@
+/*
+Instructions:
+Write a function called "isLessThan30".
+
+Given a number, "isLessThan30" returns whether it is less than 30.
+
+Input:
+isLessThan30(9);
+
+Output:
+true
+
+Starter Code:
+function isLessThan30(num){
+	// your code here
+}
+*/
+
+// Answer:
+function isLessThan30(num){
+	if(num < 30){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+isLessThan30(9);
+
+// Output
+true
