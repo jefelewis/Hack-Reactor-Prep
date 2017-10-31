ad74aa5037bd51a089e195f876f43227acdd20c3
Jeff Lewis <jeffylew77@gmail.com>
2017-10-30 17:56:20 -0700

    Create 014_Equals10.js

---
+++ Module 1/014_Equals10.js
@@ -0,0 +1,32 @@
+/*
+Instructions:
+Write a function called "equalsTen".
+
+Given a number, "equalsTen" returns whether or not the given number is 10.
+
+Input:
+equalsTen(9);
+
+Output:
+false
+
+Starter Code:
+function equalsTen(num){
+	// your code here
+}
+*/
+
+// Answer:
+function equalsTen(num){
+	if(num == 10){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+equalsTen(11);
+
+// Output
+false
