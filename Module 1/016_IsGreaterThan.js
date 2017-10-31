5de503f6070acbd2453b33cc029a36f2302c0ce0
Jeff Lewis <jeffylew77@gmail.com>
2017-10-30 18:09:11 -0700

    Create 016_IsGreaterThan.js

---
+++ Module 1/016_IsGreaterThan.js
@@ -0,0 +1,32 @@
+/*
+Instructions:
+Write a function called "isGreaterThan".
+
+Given 2 numbers, "isGreaterThan" returns whether num2 is greater than num1.
+
+Input:
+isGreaterThan(11, 10);
+
+Output:
+false
+
+Starter Code:
+function isGreaterThan(num1, num2){
+	// your code here
+}
+*/
+
+// Answer:
+function isGreaterThan(num1, num2){
+	if(num1 < num2){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+isGreaterThan(11,10);
+
+// Output
+false
