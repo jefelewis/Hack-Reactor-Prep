b63079500f1abd529e1d0ddf1f897e5738d3fd2e
Jeff Lewis <jeffylew77@gmail.com>
2017-10-30 18:00:49 -0700

    Create 015_IsLessThan.js

---
+++ Module 1/015_IsLessThan.js
@@ -0,0 +1,32 @@
+/*
+Instructions:
+Write a function called "isLessThan".
+
+Given 2 numbers, "isLessThan" returns whether num2 is less than num1.
+
+Input:
+isLessThan(9, 4);
+
+Output:
+true
+
+Starter Code:
+function isLessThan(num1, num2){
+	// your code here
+}
+*/
+
+// Answer:
+function isLessThan(num1, num2){
+	if(num1 > num2){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+isLessThan(4,2);
+
+// Output
+true
