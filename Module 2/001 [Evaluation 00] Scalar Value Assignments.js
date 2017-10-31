4c39bbd61dd44477f86b0c9199d6a61eec2a1277
Jeff Lewis <jeffylew77@gmail.com>
2017-10-30 19:13:26 -0700

    Create 001 [Evaluation 00] Scalar Value Assignments.js

---
+++ Module 2/001 [Evaluation 00] Scalar Value Assignments.js
@@ -0,0 +1,33 @@
+/*
+Instructions:
+Evaluate each line below as though YOU were the interpreter.
+￼
+
+What is the final value of x? Set the property in the "answer" object accordingly!
+
+IMPORTANT: **DO NOT** just type this out manually then paste it into a terminal so the computer does the work for you.  That won't help you learn!! 
+
+var x = 7;
+var y = 8;
+var z = 9;
+
+x = y + z;
+z = x;
+x = y;
+z = x + y;
+y = z - x;
+z = y;
+x = y * z;
+z = y/z;
+x = (z - x)/(y + z);
+
+
+Starter Code:
+var answer = {
+	finalValueOfX: null 
+	// fill this in with your MENTALLY evaluated answer.
+};
+*/
+
+// Answer:
+- 7
