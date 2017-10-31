307caba035ddd974b5694161a2612686e2278346
Jeff Lewis <jeffylew77@gmail.com>
2017-10-30 18:10:16 -0700

    Create 017_IsEqualTo.js

---
+++ Module 1/017_IsEqualTo.js
@@ -0,0 +1,33 @@
+/*
+Instructions:
+Write a function called "isEqualTo".
+
+Given 2 numbers, "isEqualTo" returns whether num2 is equal to num1.
+
+Input:
+isEqualTo(11, 10);
+
+Output:
+false
+
+
+Starter Code:
+function isEqualTo(num1, num2){
+	// your code here
+}
+*/
+
+// Answer:
+function isEqualTo(num1, num2){
+	if(num1 == num2){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+isEqualTo(11, 10);
+
+// Output
+false
