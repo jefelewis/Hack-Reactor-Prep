f2b18ee9f49174360dfe97dc07630c48a41238f3
Jeff Lewis <jeffylew77@gmail.com>
2017-10-30 17:48:19 -0700

    Create 012_IsGreaterThanTen.js

---
+++ Module 1/012_IsGreaterThanTen.js
@@ -0,0 +1,33 @@
+/*
+Instructions:
+Write a function called "isGreaterThanTen".
+
+Given a number, "isGreaterThanTen" returns whether it is greater than 10.
+
+Input:
+isGreaterThanTen(11);
+
+Output:
+true
+
+
+Starter Code:
+function isGreaterThanTen(num){
+	// your code here
+}
+*/
+
+// Answer:
+function isGreaterThanTen(num){
+	if(num > 10){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+isGreaterThanTen(11);
+
+// Output
+true
