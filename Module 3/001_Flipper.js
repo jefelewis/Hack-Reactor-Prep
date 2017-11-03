388c43cb9e28027ebc644b6e1ba51a3935d67cb1
Jeff Lewis <jeffylew77@gmail.com>
2017-11-03 14:06:00 -0700

    Update 001_Flipper.js

--- Module 3/001_Flipper.js
+++ Module 3/001_Flipper.js
@@ -66,3 +66,6 @@ function flipPairs(input){
 }
  
 flipPairs("check out how interesting this problem is, it's insanely interesting!");
+
+// Output
+"hcce kuo toh wnietertsni ghtsip orlbmei ,si \'t sniasenyli tnreseitgnundefined!"
