65103c2f3109726db6ad93e55d51d0d82c34231f
Jeff Lewis <jeffylew77@gmail.com>
2017-11-02 18:35:01 -0700

    Create 028_Cube.js

---
+++ Module 1/028_Cube.js
@@ -0,0 +1,28 @@
+/*
+Instructions:
+Write a function called "cube".
+
+Given a number, "cube" returns the cube of that number.
+
+Input:
+cube(3);
+
+Output:
+27
+
+
+Starter Code:
+function cube(num){
+	// your code here
+}
+*/
+
+// Answer:
+function cube(num){
+	return num * num * num;
+}
+
+cube(3);
+
+// Output
+27
