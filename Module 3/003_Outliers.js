82d4236865a7f76a6f3a56eafd34a1c1f28c1b42
Jeff Lewis <jeffylew77@gmail.com>
2017-11-03 16:22:34 -0700

    Create 003_Outliers.js

---
+++ Module 3/003_Outliers.js
@@ -0,0 +1,53 @@
+/*
+PLEASE FOLLOW THESE INSTRUCTIONS CAREFULLY.
+
+1) Prepare to record & time:
+Prepare your screencasting (both video and audio) setup all ready to go before de-obfuscating the problem statement.
+Prepare an audible countdown timer (say, the Clock app on your phone).
+
+2) Begin the problem:
+Go to http://www.rot13.com/ and de-obfuscate the problem statement.
+Paste the problem statement into the work area in Repl.it (replace the original, obfuscated version).
+Start your timer for: 20 minutes / TWENTY MINUTES
+Start recording.
+
+3) Do the problem:
+Follow the instructions from the sidebar titled "How to Run Your Interview".
+Note: Since you are working by yourself, you have to mimic the live experience by asking rhetorical questions, then provisionally answering them yourself on behalf of your imaginary live interviewer. Ask those questions out loud anyway! 
+
+4) Finish but DO NOT upload:
+When you have a working solution that you are satisfied with OR when the timer goes off...
+Stop recording.
+
+5) Review your own work and record your notes here:
+Watch your video and observe what you did well and what you want to improve for next time.
+Write those notes here.
+
+*** DO NOT WAIT FOR STAFF FEEDBACK -- PROCEED TO THE NEXT PROBLEM ***
+
+Starter Code :
+Problem description (see instructions for how to de-obfuscate).
+
+Tvira n fgevat bs rira naq bqq ahzoref, svaq juvpu vf gur fbyr rira ahzore be gur fbyr bqq ahzore.
+Gur erghea inyhr fubhyq or 1-vaqrkrq, abg 0-vaqrkrq.
+
+Rknzcyrf :
+qrgrpgBhgyvreInyhr("2 4 7 8 10"); // => 3 - Guveq ahzore vf bqq, juvyr gur erfg bs gur ahzoref ner rira
+qrgrpgBhgyvreInyhr("1 10 1 1");  //=> 2 - Frpbaq ahzore vf rira, juvyr gur erfg bs gur ahzoref ner bqq
+
+
+De-obfuscated Instructions:
+Given a string of even and odd numbers, find which is the sole even number or the sole odd number.
+The return value should be 1-indexed, not 0-indexed.
+
+Examples :
+detectOutlierValue("2 4 7 8 10"); // => 3 - Third number is odd, while the rest of the numbers are even
+detectOutlierValue("1 10 1 1");  //=> 2 - Second number is even, while the rest of the numbers are odd
+*/
+
+function detectOutlierValue(input){
+  // Your code here
+}
+
+detectOutlierValue("2 4 7 8 10");
+detectOutlierValue("1 10 1 1");
