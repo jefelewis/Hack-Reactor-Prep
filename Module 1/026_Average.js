d6b5ebe3ead11ce690524bff5c68fbe1425516aa
Jeff Lewis <jeffylew77@gmail.com>
2017-11-02 18:32:07 -0700

    Create 026_Average.js

---
+++ Module 1/026_Average.js
@@ -0,0 +1,28 @@
+/*
+Instructions:
+Write a function called "average".
+
+Given two numbers, "average" returns their average.
+
+Input:
+average(4, 6);
+
+Output:
+5
+
+
+Starter Code:
+function average(num1, num2){
+	// your code here
+}
+*/
+
+// Answer:
+function average(num1, num2){
+	return (num1 + num2)/2;
+}
+
+average(4,6);
+
+// Output
+5
