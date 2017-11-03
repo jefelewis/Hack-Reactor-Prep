4c5d853b8a1a160c904b61892c16a8de83a29c04
Jeff Lewis <jeffylew77@gmail.com>
2017-11-02 18:36:00 -0700

    Create 029_Square.js

---
+++ Module 1/029_Square.js
@@ -0,0 +1,28 @@
+/*
+Instructions:
+Write a function called "square".
+
+Given a number, "square" should return the square of the given number.
+
+Input:
+square(5);
+
+Output:
+25
+
+
+Starter Code:
+function square(num){
+	// your code here
+}
+*/
+
+// Answer:
+function square(num){
+	return num * num;
+}
+
+square(5);
+
+// Output
+25
