5581dcee068b5a5a59c85954df96a3896974c6f3
Jeff Lewis <jeffylew77@gmail.com>
2017-11-06 08:05:10 -0800

    Create 004 Transpose.js

---
+++ Module 3-V2/004 Transpose.js
@@ -0,0 +1,21 @@
+/*
+Instructions:
+You will be given an array that contains two strings. Your job is to create a function that will take those two strings and transpose them, so that the strings go from top to bottom instead of left to right.
+e.g. transposeTwoStrings(["Hello","World"]);
+
+should return:
+H W
+e o
+l r
+l l
+o d
+*/
+
+// Input: Array with two strings
+// Output: String
+// Process: Iterate through the strings --> return array 
+
+function transposeTwoStrings(arr){
+  // Your code here
+
+}
