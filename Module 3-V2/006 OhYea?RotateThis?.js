e65b54a21eefba90e3345cae40782e266eebd139
Jeff Lewis <jeffylew77@gmail.com>
2017-11-06 08:06:02 -0800

    Create 006 OhYea?RotateThis?.js

---
+++ Module 3-V2/006 OhYea?RotateThis?.js
@@ -0,0 +1,20 @@
+/*
+Instructions:
+Is one string a rotated version of another?
+
+For example:
+String 1: "hello world"
+String 2: "orldhello w"
+
+If you double the string, you'll be to trivially find another string inside it using regular String methods.
+
+Doubled string: "hello worldhello world"
+Search w/in it: "       orldhello w    "
+
+
+*/
+
+function isRotated(str1, str2) {
+  // Your code here
+
+}
