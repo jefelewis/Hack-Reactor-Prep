edac3011a3499560ee5b892f0c216206039e73bd
Jeff Lewis <jeffylew77@gmail.com>
2017-11-07 12:10:04 -0800

    Update 004 Transpose.js

--- Module 3-V2/004 Transpose.js
+++ Module 3-V2/004 Transpose.js
@@ -3,33 +3,35 @@ Instructions:
 You will be given an array that contains two strings. Your job is to create a function that will take those two strings and transpose them, so that the strings go from top to bottom instead of left to right.
 e.g. transposeTwoStrings(["Hello","World"]);
 
-should return:
+Should return:
 H W
 e o
 l r
 l l
 o d
+
 */
 
 // Input: Array with two strings
 // Output: String
-// Process: Create output array --> find the max length in the instance one word is longer than the other --> Iterate through the strings --> Assign characters to output array and add spaces " " if one string is longer than the other string --> Return output 
+// Process: Create maxLength variable --> find the max length in the instance one word is longer than the other --> Iterate through the strings --> Create variables for both elements and assign them with spaces " " --> Assign characters to elements and add spaces " " if one string is longer than the other string --> Return output 
 
 function transposeTwoStrings(arr) {
   // Create maxLength variable
-  var size = 0;
-  // Find the maxLength
+  var maxLength = 0;
+  // Find the maxLength between the two strings
   if(arr[0].length > arr[1].length){
-    size = arr[0].length
+    maxLength = arr[0].length
   }
   else{
-    size = arr[1].length
+    maxLength = arr[1].length
   }
-
-  for(var i = 0; i < size; i++){
+  // Iterate through the array items
+  for(var i = 0; i < maxLength; i++){
+    // Create element variables
     var element1 = " ";
     var element2 = " ";
-
+    // Assign characters to the element
     if(typeof arr[0][i] !== "undefined"){
       element1 = arr[0][i];
     }
