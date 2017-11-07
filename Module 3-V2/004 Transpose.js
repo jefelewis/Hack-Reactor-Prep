c0b14fe912e82b83ca92297b2f556d4fb080b3a5
Jeff Lewis <jeffylew77@gmail.com>
2017-11-07 11:16:04 -0800

    Update 004 Transpose.js

--- Module 3-V2/004 Transpose.js
+++ Module 3-V2/004 Transpose.js
@@ -15,21 +15,41 @@ o d
 // Output: String
 // Process: Create output array --> find the max length in the instance one word is longer than the other --> Iterate through the strings --> Assign characters to output array and add spaces " " if one string is longer than the other string --> Return output 
 
-function transposeTwoStrings(arr){
-  // Create results array
-  var results = [];
-  // Find the max length
-  var maxLength = arr[0].length;
-  if(arr[1].length > arr[0].length){
-    maxLength = arr[1].length;
+function transposeTwoStrings(arr) {
+  // Create maxLength variable
+  var size = 0;
+  // Find the maxLength
+  if(arr[0].length > arr[1].length){
+    size = arr[0].length
   }
-  // Iterate through the array
-  for(var i = 0; i < maxLength; i++){
-    // Push values from both strings with consideration for spaces " " if one string is shorter than the other
-    results.push((arr[0][i] || " ") + (arr[1][i] ||  " "))
+  else{
+    size = arr[1].length
   }
-  // Return output
-  return results.join("\n");
+
+  for(var i = 0; i < size; i++){
+    var element1 = " ";
+    var element2 = " ";
+
+    if(typeof arr[0][i] !== "undefined"){
+      element1 = arr[0][i];
+    }
+    if(typeof arr[1][i] !== "undefined"){
+      element2 = arr[1][i];
+    }
+    
+    // Output
+    console.log(element1 + " " + element2);
+  }
 }
 
 transposeTwoStrings(["Hello","World"]);
+
+/* Output --> 
+
+H W
+e o
+l r
+l l
+o d
+
+*/
