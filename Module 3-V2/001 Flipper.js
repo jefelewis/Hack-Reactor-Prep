bc673f1228b83523617ee609aa3107bef092902b
Jeff Lewis <jeffylew77@gmail.com>
2017-11-08 16:17:14 -0800

    Update 001 Flipper.js

--- Module 3-V2/001 Flipper.js
+++ Module 3-V2/001 Flipper.js
@@ -18,7 +18,7 @@ function flipPairs(input){
 	// Create Output String
 	var output = "";
 	// Iterate through the string
-	for(var i = 0; i < input.length; i += 2){
+	for(var i = 0; i < input.length - 1; i += 2){
 		// Flip the second character to be the first character
 		output += input[i + 1];
 		// Flip the first character to be the second character
@@ -30,4 +30,4 @@ function flipPairs(input){
 
 flipPairs("check out how interesting this problem is, it\'s insanely interesting!");
 
-// Output --> "hcce kuo toh wnietertsni ghtsip orlbmei ,si \'t sniasenyli tnreseitgnundefined!"
+// Output --> "hcce kuo toh wnietertsni ghtsip orlbmei ,si \'t sniasenyli tnreseitgn!"
