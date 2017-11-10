88a3023d1e5f245db1c10df91b9deea6df792747
Jeff Lewis <jeffylew77@gmail.com>
2017-11-09 19:27:41 -0800

    Update 007 DivideAndConquer.js

--- Module 3-V2/007 DivideAndConquer.js
+++ Module 3-V2/007 DivideAndConquer.js
@@ -1,7 +1,7 @@
 /*
-"Binary Search" is a technique for very rapidly searching a sorted collection by cutting the search space in half at every pass.
+Binary search is a technique for very rapidly searching a sorted collection by cutting the search space in half at every pass.
 
-Given a SORTED array, such as this:
+Given a sorted array, such as this:
 [1, 3, 16, 22, 31, 33, 34]
 
 You can search for the value 31, as follows:
@@ -28,32 +28,31 @@ Process: Take the array length --> Divide the length by 2 --> If the remainder i
 */
 
 
-function search(array, value){
-  // Create variable for start index
-  var startIndex = 0;
-  // Create variable for end index
-  var endIndex = array.length - 1;
+function binarySearch(arr, target){
+	// Define start and end index
+	var startIndex = 0;
+	var endIndex = arr.length - 1;
 
-  //
-  while(startIndex <= endIndex){
-    // Crate a variable for the mid index
-    var midIndex = Math.floor((startIndex + endIndex) / 2);
-    // If midpoint equals the value, return the value/midpoint
-    if(array[midIndex] === value){
-      return midIndex;
-    }
-    // Search to the left
-    else if(array[midIndex] > value){
-      endIndex = midIndex - 1;
-    }
+	// Create While Loop
+	while(startIndex <= endIndex){
+		// Define midIndex
+		var midIndex = Math.floor((startIndex + endIndex) / 2);
 
-    // Search to the right
-    else {
-      startIndex = midIndex + 1;
-    }
-  }
-  // Return null if value is not found
-  return null;
+		// Find if there is match
+		if(arr[midIndex] === target){
+			return midIndex;
+		}
+		// Search Right
+		else if(target > midIndex){
+			startIndex = midIndex + 1;
+		}
+		// Search Left
+		else{
+			endIndex = midIndex - 1;
+		}
+	}
+	// Return null if not found
+	return null;
 }
 
 search([1, 3, 16, 22, 31, 33, 34], 31);
