8728d471b871755966d4b68395ea9a17fbd38c0b
Jeff Lewis <jeffylew77@gmail.com>
2017-11-09 19:28:13 -0800

    Update 006 OhYea?RotateThis?.js

--- Module 3-V2/006 OhYea?RotateThis?.js
+++ Module 3-V2/006 OhYea?RotateThis?.js
@@ -10,24 +10,26 @@ If you double the string, you'll be to trivially find another string inside it u
 
 Doubled string: "hello worldhello world"
 Search w/in it: "       orldhello w    "
+
 */
 
 /*
-Input: 2 strings
+Input: String
 Output: Boolean
-Process: Create a large string with string 1 copied 3 times (2 might not work for all instances) --> Use the .includes() prototype to find string 2 in the large string --> Output true if it includes it. Otherwise, output false
+Process: Create tripled string of string 1 to search from --> Use the include JavaScript Method to find string 2 within string 1 --> return true or false
 */
 
 function isRotated(str1, str2) {
-  // Create large string to search from
-  var searchString = str1 + str1 + str1;
-  // Search through large string for string 2
-  if(searchString.includes(str2)){
-    return true;
-  }
-  else{
-    return false;
-  }
+  // Create Tripled String
+  var tripledString = str1 + str1 + str1;
+	
+	// Use include method to find the string
+	if(tripledString.includes(str2)){
+		return true;
+	}
+	else{
+		return false
+	}
 }
 
 isRotated("hello world", "orldhello w");
